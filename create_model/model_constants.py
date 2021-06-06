8eeb48d21515cc38537a8394081b01ff04facf47
mario11-wiet <mk1103@wp.pl>
2021-06-06 07:09:37 +0200

    Save model

--- create_model/model_constants.py
+++ create_model/model_constants.py
@@ -1,4 +1,4 @@
-ML_MODEL_FILENAME = '../save_model/myModel.h5'
+ML_MODEL_FILENAME = '../image_recognition/save_model/myModel.h5'
 numer_class = 10
 optimizer = 'rmsprop'
 epoch = 25
--- image_recognition/main.py
+++ image_recognition/main.py
@@ -1,15 +1,26 @@
+from pprint import pprint
 import numpy as np
-from keras.preprocessing import image
 import constants as c
+from keras.models import load_model
+from keras.preprocessing import image
+model = load_model('save_model/myModel.h5')
+
 
 def main():
-    #wczytanie zdjecia w rozmiarze
+    # wczytanie zdjecia w rozmiarze
     img = image.load_img(c.path, target_size=(32, 32))
-    #przetwarzanie zdjecia
+    img_l = img.convert("L")
+    img_s = img.convert("1")
+    img.show()
+    img_s.show()
+    img_l.show()
+    # przetwarzanie zdjecia
     processing = image.img_to_array(img)
     processing = np.expand_dims(processing, axis=0)
-    category = c.model.predict(processing)
-
+    category = model.predict(processing)
+    category = list(enumerate(category[0]))
+    category = sorted(category, key=lambda x: x[1], reverse=True)
+    pprint(c.arr_category[category[0][0]])
 
 
 if __name__ == '__main__':
